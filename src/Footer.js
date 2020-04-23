ccc47e97cb7a07102770e863f69ad34f204225d1
Minh Tuấn Bùi <buiminhtuan1507@gmail.com>
2020-04-23 09:37:12 +0700

    five commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,48 +1,63 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaFacebook} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaYoutube} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+
 export default class Footer extends Component {
    render() {
       return (
-         <div class="container text-center">
+         <div className="container text-center">
             
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="col-md-3">
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Về chúng tôi</a></li>
-                            <li><a href="#">Tin tức</a></li>
-                        </ul>
+            <div className="row">
+                <div className = "col-lg-4">
+                    <div>SACHVIET.COM</div>
+                    <div>Địa chỉ: Phường Bách Khoa, Quận Hai Bà Trưng , Hà Nội </div>
+                    <div>SachViet.com nhận đặt hàng trực tuyến và giao hàng tận nơi. KHÔNG hỗ trợ đặt mua và nhận hàng trực tiếp tại văn phòng cũng như tất cả Hệ Thống SachViet trên toàn quốc
+
                     </div>
-                    <div class="col-md-3">
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Danh mục 1</a></li>
-                            <li><a href="#">Danh mục 2</a></li>
-                            
-                        </ul>
-                    </div>
-                    <div class="col-md-3">
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Web analytics</a></li>
-                            <li><a href="#">Presentations</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3">
-                        <ul class="nav nav-pills nav-stacked">
-                            <li><a href="#">Product Help</a></li>
-                            <li><a href="#">Developer API</a></li>
-                        </ul>
-                    </div>
+                    <div>
+                       
+                        <a href ="/#"> <FaFacebook className = "col-2" size = "30" color ="blue"/></a>
+                        <a href ="/#"><FaInstagram className = "col-3" size = "30" color = "red"/></a>
+                        <a href ="/#"> <FaYoutube className = "col-3" size = "30" color = "red"/></a>
+                        <a href = "/#"> <FaTwitter className = "col-3" size = "30" color = "blue"/></a>
+                       
+                        </div>
+                </div>
+                <div className ="col-lg-2">
+                   
+                        <div>Dịch vụ</div>
+                        <div>Điều khoản sử dụng</div>
+                        <div>Chính sách bảo mật</div>
+                        <div>Giới thiệu</div>
+                        <div>Hệ thống nhà sách</div>
+                   
+                   
+                   
+                </div>
+                <div className="col-lg-3">
+                
+                        <divdiv>Hỗ trợ</divdiv>
+                        <div>Phương thức thanh toán</div>
+                        <div>Phương thức vận chuyển</div>
+                        <div>Chính sách đổi trả</div>
+                        
+                    
+                </div>
+                <div className = "col-lg-3">
+                    <div>Tài khảon của tôi</div>
+                    <div>Đăng nhập</div>
+                    <div>Đăng ký</div> 
+                    <div>Lịch sử mua hàng</div>
                 </div>
             </div>
-            
-            <div class="row">
-                <div class="col-lg-4">
-                    <ul class="nav nav-pills nav-justified">
-                        <li><a href="/">© 2018 Company Name.</a></li>
-                        <li><a href="#">Thông tin dịch vụ</a></li>
-                        <li><a href="#">Bản quyền</a></li>
-                    </ul>
+            <div className = "row">
+                <div>
+                    <br></br>
+                    <br></br>
                 </div>
             </div>
         </div>
